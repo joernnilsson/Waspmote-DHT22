70c757cd9557b24a4a0f63a5abc832268a0182d4
Jørn Sandvik Nilsson <joern.nilsson@gmail.com>
2014-09-24 19:09:42 +0200

    changed example

--- examples/Serial/Serial.ino
+++ examples/Serial/Serial.ino
@@ -1,39 +1,40 @@
-#include <DHT22.h>
-// Only used for sprintf
-#include <stdio.h>
 
-// Data wire is plugged into port 7 on the Arduino
-// Connect a 4.7K resistor between VCC and the data pin (strong pullup)
-#define DHT22_PIN 7
+#include <DHT22.h>     
 
-// Setup a DHT22 instance
-DHT22 myDHT22(DHT22_PIN);
 
-void setup(void)
-{
-  // start serial port
-  Serial.begin(9600);
-  Serial.println("DHT22 Library Demo");
+DHT22 myDHT22(DIGITAL4);
+
+// Signal pin on DIGITAL4, power pin on DIGITAL3
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(DIGITAL3, OUTPUT);
+    digitalWrite(DIGITAL3, HIGH);
+    
+      USB.ON();
+      delay(100);
+      
+      USB.println("Booting");
 }
 
-void loop(void)
-{ 
-  DHT22_ERROR_t errorCode;
+
+void loop() {
+   DHT22_ERROR_t errorCode;
   
   // The sensor can only be read from every 1-2s, and requires a minimum
   // 2s warm-up after power-on.
   delay(2000);
   
-  Serial.print("Requesting data...");
+    USB.println("Requesting data...");
   errorCode = myDHT22.readData();
   switch(errorCode)
   {
     case DHT_ERROR_NONE:
-      Serial.print("Got Data ");
-      Serial.print(myDHT22.getTemperatureC());
-      Serial.print("C ");
-      Serial.print(myDHT22.getHumidity());
-      Serial.println("%");
+      USB.print("Got Data ");
+      USB.print(myDHT22.getTemperatureC());
+      USB.print("C ");
+      USB.print(myDHT22.getHumidity());
+      USB.println("%");
       // Alternately, with integer formatting which is clumsier but more compact to store and
 	  // can be compared reliably for equality:
 	  //	  
@@ -41,32 +42,34 @@ void loop(void)
       sprintf(buf, "Integer-only reading: Temperature %hi.%01hi C, Humidity %i.%01i %% RH",
                    myDHT22.getTemperatureCInt()/10, abs(myDHT22.getTemperatureCInt()%10),
                    myDHT22.getHumidityInt()/10, myDHT22.getHumidityInt()%10);
-      Serial.println(buf);
+      USB.println(buf);
       break;
     case DHT_ERROR_CHECKSUM:
-      Serial.print("check sum error ");
-      Serial.print(myDHT22.getTemperatureC());
-      Serial.print("C ");
-      Serial.print(myDHT22.getHumidity());
-      Serial.println("%");
+      USB.print("check sum error ");
+      USB.print(myDHT22.getTemperatureC());
+      USB.print("C ");
+      USB.print(myDHT22.getHumidity());
+      USB.println("%");
       break;
     case DHT_BUS_HUNG:
-      Serial.println("BUS Hung ");
+      USB.println("BUS Hung ");
       break;
     case DHT_ERROR_NOT_PRESENT:
-      Serial.println("Not Present ");
+      USB.println("Not Present ");
       break;
     case DHT_ERROR_ACK_TOO_LONG:
-      Serial.println("ACK time out ");
+      USB.println("ACK time out ");
       break;
     case DHT_ERROR_SYNC_TIMEOUT:
-      Serial.println("Sync Timeout ");
+      USB.println("Sync Timeout ");
       break;
     case DHT_ERROR_DATA_TIMEOUT:
-      Serial.println("Data Timeout ");
+      USB.println("Data Timeout ");
       break;
     case DHT_ERROR_TOOQUICK:
-      Serial.println("Polled to quick ");
+      USB.println("Polled to quick ");
       break;
   }
+  
 }
+
